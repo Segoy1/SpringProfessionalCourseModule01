46fefd8fa74f11b0b223098acb2766b83abfe6e6
Segoy <segoy.thiel@protonmail.com>
2023-10-17 09:29:45 +0200

    done Question 1

--- module01-question01/src/main/java/com/spring/professional/exam/tutorial/module01/question01/no/dependency/injection/EmployeesSalariesReportService.java
+++ module01-question01/src/main/java/com/spring/professional/exam/tutorial/module01/question01/no/dependency/injection/EmployeesSalariesReportService.java
@@ -5,10 +5,20 @@ import com.spring.professional.exam.tutorial.module01.question01.commons.dao.Emp
 import com.spring.professional.exam.tutorial.module01.question01.commons.ds.Employee;
 import com.spring.professional.exam.tutorial.module01.question01.commons.ds.EmployeeSalary;
 import com.spring.professional.exam.tutorial.module01.question01.commons.report.PdfSalaryReport;
+import com.spring.professional.exam.tutorial.module01.question01.commons.report.SalaryReport;
+import com.spring.professional.exam.tutorial.module01.question01.commons.report.XlsSalaryReport;
 
 import java.util.List;
 
+
 class EmployeesSalariesReportService {
+
+    private final SalaryReport salaryReport;
+
+    public EmployeesSalariesReportService(SalaryReport salaryReport) {
+        this.salaryReport = salaryReport;
+    }
+
     void generateReport() {
         EmployeeDao employeeDao = new EmployeeDao();
         List<Employee> employees = employeeDao.findAll();
@@ -16,7 +26,6 @@ class EmployeesSalariesReportService {
         EmployeeSalaryCalculator employeeSalaryCalculator = new EmployeeSalaryCalculator();
         List<EmployeeSalary> employeeSalaries = employeeSalaryCalculator.calculateSalaries(employees);
 
-        PdfSalaryReport pdfSalaryReport = new PdfSalaryReport();
-        pdfSalaryReport.writeReport(employeeSalaries);
+        salaryReport.writeReport(employeeSalaries);
     }
 }
